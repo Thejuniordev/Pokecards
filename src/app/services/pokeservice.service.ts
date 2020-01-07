ae656577c8af33b7995e2869e23a66e7a2571c03
Darcy Alves dos Santos Junior <darcy.santos@compasso.com.br>
2020-01-07 10:37:12 -0300

    Create services

---
+++ src/app/services/pokeservice.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeserviceService {
+
+  cardsUrl = 'https://api.pokemontcg.io/v1/cards/';
+
+  constructor(private http: HttpClient) { }
+
+  cardsListing(): Observable<any> {
+    return this.http.get(`${this.cardsUrl}`);
+  }
+
+  cardsDetails(cardId) {
+    return this.http.get(`${this.cardsUrl}/${cardId}`);
+  }
+}
